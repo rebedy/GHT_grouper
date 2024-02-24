d31f8682fcf7353f16b8f9636ab65280a4d41884
rebedy <dyan.lee717@gmail.com>
2024-02-24 23:59:53 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ght_grouper
+
+To manage Glaucoma Hemifield Test data.
+This script is for sorting the GHT image files into groups based on the criteria.
+The criteria is a column of the dataframe.
+The script will create folders and move the image files into the folders based on the criteria.
+The script will also create a progress bar to show the progress of the sorting process.
+The script will also create a dataframe and save it as an excel file.
---
+++ ght_grouper.py
@@ -0,0 +1,60 @@
+__author__ = "Da young Lee"
+__credits__ = ["Seung hyup Hyun", "Jong chul Han", "Da young Lee"]
+__version__ = "1.0.0"
+__date__ = "2018.05.04"
+__maintainer__ = "Da young Lee"
+__email__ = "dyan.lee717@gmail.com"
+__status__ = "Unofficial"
+
+import sys, os
+import glob, shutil
+import pandas as pd
+
+data_dirr = 'GHT_image/'
+data_list = glob.glob(data_dirr+'*.jpg')
+data_len = len(data_list)
+print('》 Image data directory is, "', data_dirr,'"')
+print('》 There are  (',data_len,')  image files in data directory.  \n')
+xlsx_path = 'result.xlsx'
+df = pd.read_excel(xlsx_path,index_col=False, columns = False)
+
+file_name = df['FileName']
+print("》 Please Enter a name of column as criteria for sorting.")
+print("    (If 'KeyError' occurs,")
+print("        check the typed value matches column name.)" )
+col_name = input('Column >>> ')    
+
+
+def mkdir_path(folder_dir):
+    if not os.path.exists(folder_dir):
+        os.mkdir(folder_dir)
+    return folder_dir
+
+
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(round(length * iteration // float(total)))
+    bar = fill * filledLength + '-' * (length - filledLength)
+    # print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percent, '%', suffix))#, end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        sys.stdout.write('\n')
+        # print()
+    sys.stdout.flush()
+
+
+print('\n')
+print('》 Grouping images... ')
+target_col = df[col_name]
+for i, value in enumerate(target_col):   
+    gr_dir = os.path.join(data_dirr+value+'\\')
+    os.makedirs(gr_dir, exist_ok=True)    
+    shutil.copy(data_dirr+file_name[i], gr_dir+file_name[i])
+    printProgressBar(i, len(target_col)-1, 'Sorting: ', 'Complete!', 1, 60)
+    if data_len-i-1 == 0:
+        break
+
+print('》 Done. \n')
+
+input('Press ENTER key to exit >>> ')
